6e7054beec65f73a876bd84e28db49b6bdfaa705
maeiky <maeiky@gmail.com>
2024-09-27 13:59:29 -0400

    #

--- src/main.c
+++ src/main.c
@@ -17,8 +17,11 @@ int main(int argc, char *argv[]) {
      printf("--\n");
    for (int i = 0; i< args.pathsLen; i++) {
       for(int j = 0; j < args.action_sz; j++){
-         printf("%s[%d]: '%s' \n", args.paths[i].norm_path,  args.action[j].replace_count, args.action[j].target );
+         printf("%s:(%d) '%s' \n", args.paths[i].norm_path,  args.action[j].replace_count, args.action[j].target );
          printf("%s: '%s'\n", args.paths[i].output, args.action[j].replace );
+         if(args.action[j].replace_count==0){
+             printf("error, not found: ", args.action[j].target);
+         }
          printf("--\n");
       }
    }
